d1bafae51fa6b9a1e1db875cd03c6493e8aeaa33
akke <liulefirst@163.com>
2026-04-22 18:34:38 +0800

    feat: Add http module and SessionInfoResponse tests
    - Public export http module for testing
    - Add Deserialize to SessionInfoResponse
    - Add http_api_test.rs with 10 tests for SessionInfoResponse
    
    Coverage target: working towards 80%

--- cloud-server/src/lib.rs
+++ cloud-server/src/lib.rs
@@ -1,7 +1,9 @@
 // Copyright (c) 2025 CC-Island Contributors
 // SPDX-License-Identifier: MIT
+pub mod config;
 pub mod db;
 pub mod messages;
 pub mod ws;
+pub mod http;
 
 pub use messages::{CloudMessage, DeviceInfo, HookType, ChatMessageData, MessageType};
